ecea93aa17b526da76f0d4b3ad35a1d3a0fe1bbb
itzKv <kenkenku6@gmail.com>
2025-03-07 13:51:28 +0700

    Font Family defined Globally

--- clockify/lib/core/themes/theme.dart
+++ clockify/lib/core/themes/theme.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 final ThemeData appTheme = ThemeData(
+  fontFamily: 'Nunito Sans',
   primaryColor: const Color(0xff25367B),
   scaffoldBackgroundColor: const Color(0xff25367B),
   secondaryHeaderColor: const Color(0xffF5F6FC)
