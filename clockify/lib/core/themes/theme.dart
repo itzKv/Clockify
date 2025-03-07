efe29774a5a9f28763ab9e0fd797d1718e7acf5f
itzKv <kenkenku6@gmail.com>
2025-03-07 09:21:54 +0700

    New color

--- clockify/lib/core/themes/theme.dart
+++ clockify/lib/core/themes/theme.dart
@@ -3,4 +3,5 @@ import 'package:flutter/material.dart';
 final ThemeData appTheme = ThemeData(
   primaryColor: const Color(0xff25367B),
   scaffoldBackgroundColor: const Color(0xff25367B),
+  secondaryHeaderColor: const Color(0xffF5F6FC)
 );
