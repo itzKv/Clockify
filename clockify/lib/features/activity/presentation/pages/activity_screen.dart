27a0ab817c2160a6d47680aa7ad88348ac67180e
itzKv <kenkenku6@gmail.com>
2025-03-08 12:26:22 +0700

    Init screen

---
+++ clockify/lib/features/activity/presentation/pages/activity_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class ActivityScreen extends StatefulWidget {
+  const ActivityScreen({super.key});
+
+  @override
+  State<ActivityScreen> createState() => _ActivityScreenState();
+}
+
+class _ActivityScreenState extends State<ActivityScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Expanded(
+          child: Column(
+            children: [
+              // Search
+
+              // List of History Activity
+            ],
+          ),
+        )
+      ),
+    );
+  }
+}
