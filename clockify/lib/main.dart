ab1d6f88e43d70e1db9c3230c212623639f8d5ce
itzKv <kenkenku6@gmail.com>
2025-03-11 09:46:19 +0700

    Initialize Hive

--- clockify/lib/main.dart
+++ clockify/lib/main.dart
@@ -1,17 +1,46 @@
 import 'package:clockify/core/themes/theme.dart';
+import 'package:clockify/features/activity/business/repositories/activity_repository.dart';
+import 'package:clockify/features/activity/business/usecases/get_all_activities.dart';
+import 'package:clockify/features/activity/business/usecases/save_activity.dart';
+import 'package:clockify/features/activity/data/datasources/activity_local_data_source.dart';
+import 'package:clockify/features/activity/data/models/activity_model.dart';
+import 'package:clockify/features/activity/data/repositories/activity_repository_impl.dart';
+import 'package:clockify/features/activity/presentation/pages/activity_screen.dart';
+import 'package:clockify/features/home/presentation/pages/home_screen.dart';
 import 'package:clockify/features/login/presentation/pages/login_screen.dart';
 import 'package:clockify/features/password/presentation/pages/password_screen.dart';
 import 'package:clockify/features/create_account/presentation/pages/create_account_screen.dart';
 import 'package:clockify/features/splash/presentation/pages/splash_screen.dart';
 import 'package:clockify/features/timer/presentation/pages/timer_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // await Hive.initFlutter();
-  // await Hive.openBox('appBox');
+  // setup Hive
+  await Hive.initFlutter();
+  Hive.registerAdapter(ActivityModelAdapter());
+  final activityBox =  Hive.openBox<ActivityModel>('activityBox');
 
-  runApp(const MainApp());
+  // Data source, Repository
+  final localDataSource = ActivityLocalDataSourceImpl(await activityBox);
+  final activityRepository = ActivityRepositoryImpl(localDataSource: localDataSource);
+
+  // Use cases
+  final saveActivity = SaveActivity(activityRepository); 
+  final getAllActivities = GetAllActivities(activityRepository);
+
+  runApp(
+    MultiProvider(
+      providers: [
+        Provider<ActivityRepository>.value(value: activityRepository,),
+        Provider<SaveActivity>.value(value: saveActivity),
+        Provider<GetAllActivities>.value(value: getAllActivities,)
+      ],
+      child: MainApp(),
+    )
+  );
 }
 
 class MainApp extends StatelessWidget {
@@ -26,7 +55,9 @@ class MainApp extends StatelessWidget {
         '/login': (context) => LoginScreen(),
         '/createAccount': (context) => CreateAccountScreen(),
         '/password': (context) => PasswordScreen(),
+        '/home': (context) => HomeScreen(),
         '/timer': (context) => TimerScreen(),
+        '/activity': (context) => ActivityScreen()
       },
       theme: appTheme,
     );
