05858037949d2ecbe4e39eb6aa92cc458671a298
itzKv <kenkenku6@gmail.com>
2025-03-13 16:56:30 +0700

    Add: API using Dio

---
+++ clockify/lib/core/network/api_endpoints.dart
@@ -0,0 +1,6 @@
+class ApiEndpoints {
+  static const String baseUrl = "https://light-master-eagle.ngrok-free.app/api/v1";
+  static const String login = "$baseUrl/user/login";
+  static const String register = "$baseUrl/user/register";
+  static const String verifyEmail = "$baseUrl/user/verifyemail";
+}
