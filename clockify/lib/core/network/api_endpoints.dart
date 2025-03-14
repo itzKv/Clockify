23e219fddb5fd1cb7d80285fee367597a28ee3fc
itzKv <kenkenku6@gmail.com>
2025-03-14 16:55:54 +0700

    Change into Local url (Temp)

--- clockify/lib/core/network/api_endpoints.dart
+++ clockify/lib/core/network/api_endpoints.dart
@@ -1,5 +1,5 @@
 class ApiEndpoints {
-  static const String baseUrl = "https://light-master-eagle.ngrok-free.app/api/v1";
+  static const String baseUrl = "http://192.168.1.8:3000/api/v1";
   static const String login = "$baseUrl/user/login";
   static const String register = "$baseUrl/user/register";
   static const String verifyEmail = "$baseUrl/user/verifyemail";
