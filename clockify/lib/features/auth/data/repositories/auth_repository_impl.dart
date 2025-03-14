5e21d44ce1f413a99573a562b6a6725d4026217e
itzKv <kenkenku6@gmail.com>
2025-03-14 13:26:44 +0700

    Fix: Change the response API from Login

--- clockify/lib/features/auth/data/repositories/auth_repository_impl.dart
+++ clockify/lib/features/auth/data/repositories/auth_repository_impl.dart
@@ -2,6 +2,7 @@ import 'package:clockify/core/errors/failure.dart';
 import 'package:clockify/core/params/params.dart';
 import 'package:clockify/features/auth/business/repositories/auth_repository.dart';
 import 'package:clockify/features/auth/data/datasources/auth_remote_data_source.dart';
+import 'package:clockify/features/auth/data/models/responses/login_user_response.dart';
 import 'package:clockify/features/auth/data/models/user_model.dart';
 import 'package:dartz/dartz.dart';
 
@@ -13,16 +14,23 @@ class AuthRepositoryImpl extends AuthRepository {
   });
 
   @override
-  Future<Either<Failure, UserModel>> login({required LoginParams loginParams}) {
-    // TODO: implement login
-    throw UnimplementedError();
+  Future<Either<Failure, LoginResult>> login({required LoginParams loginParams}) async {
+    try {
+      final response = await remoteDataSource.login(loginParams: loginParams);
+      return Right(LoginResult(user: response.user!, message: response.message!));
+    } catch (e) {
+      if (e is ServerFailure) {
+        return Left(ServerFailure(e.errorData, errorMessage: e.errorMessage));
+      }
+      return Left(ServerFailure(null, errorMessage: "Unexpected error"));
+    }
   }
 
   @override
   Future<Either<Failure, UserModel>> register({required RegisterParams registerParams}) async {
     try {
-      final user = await remoteDataSource.register(registerParams: registerParams);
-      return Right(user);
+      final response = await remoteDataSource.register(registerParams: registerParams);
+      return Right(response.user!);
     } catch (e) {
       if (e is ServerFailure) {
         return Left(ServerFailure(e.errorData, errorMessage: e.errorMessage));
