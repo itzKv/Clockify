959a96999623e0fafbb6728353492c021f163a92
itzKv <kenkenku6@gmail.com>
2025-03-14 13:27:43 +0700

    Fix: login reponse params

--- clockify/lib/features/auth/business/repositories/auth_repository.dart
+++ clockify/lib/features/auth/business/repositories/auth_repository.dart
@@ -1,10 +1,11 @@
 import 'package:clockify/core/errors/failure.dart';
 import 'package:clockify/core/params/params.dart';
+import 'package:clockify/features/auth/data/models/responses/login_user_response.dart';
 import 'package:clockify/features/auth/data/models/user_model.dart';
 import 'package:dartz/dartz.dart';
 
 abstract class AuthRepository {
-  Future<Either<Failure, UserModel>> login({ required LoginParams loginParams });
+  Future<Either<Failure, LoginResult>> login({ required LoginParams loginParams });
   Future<Either<Failure, UserModel>> register({ required RegisterParams registerParams });
   Future<Either<Failure, String>> verifyEmail({ required String emailToken });
 }
--- clockify/lib/features/auth/business/usecases/login.dart
+++ clockify/lib/features/auth/business/usecases/login.dart
@@ -1,7 +1,7 @@
 import 'package:clockify/core/errors/failure.dart';
 import 'package:clockify/core/params/params.dart';
 import 'package:clockify/features/auth/business/repositories/auth_repository.dart';
-import 'package:clockify/features/auth/data/models/user_model.dart';
+import 'package:clockify/features/auth/data/models/responses/login_user_response.dart';
 import 'package:dartz/dartz.dart';
 
 class Login {
@@ -9,7 +9,7 @@ class Login {
 
   Login(this.repository);
 
-  Future<Either<Failure, UserModel>> call(LoginParams loginParams) async {
+  Future<Either<Failure, LoginResult>> call(LoginParams loginParams) async {
     return await repository.login(loginParams: loginParams);
   }
 }
