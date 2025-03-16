5329b9bfd67e3e3c54d7299db7d64192ab153d65
itzKv <kenkenku6@gmail.com>
2025-03-16 23:51:52 +0700

    Add: get all Activity use case

--- clockify/lib/features/activity/business/usecases/get_all_activities.dart
+++ clockify/lib/features/activity/business/usecases/get_all_activities.dart
@@ -1,12 +1,14 @@
+import 'package:clockify/core/errors/failure.dart';
 import 'package:clockify/features/activity/business/entities/activity_entity.dart';
 import 'package:clockify/features/activity/business/repositories/activity_repository.dart';
+import 'package:dartz/dartz.dart';
 
 class GetAllActivities {
   final ActivityRepository repository;
 
   GetAllActivities(this.repository);
 
-  Future<List<ActivityEntity>> call() async {
-    return repository.getAllActivities();
+  Future<Either<Failure, List<ActivityEntity>>> call() async {
+    return await repository.getAllActivities();
   }
 }
