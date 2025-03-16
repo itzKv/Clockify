0e41b5528e8d20e0ddfe850fff4287c4f49b944a
itzKv <kenkenku6@gmail.com>
2025-03-16 23:53:34 +0700

    Add: connectivity_plus to check connection

--- clockify/macos/Flutter/GeneratedPluginRegistrant.swift
+++ clockify/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,11 +6,13 @@ import FlutterMacOS
 import Foundation
 
 import app_links
+import connectivity_plus
 import geolocator_apple
 import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AppLinksMacosPlugin.register(with: registry.registrar(forPlugin: "AppLinksMacosPlugin"))
+  ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
