ecfca014140b76a6b48c3fa30e38cdad2dc50596
itzKv <kenkenku6@gmail.com>
2025-03-19 16:41:38 +0700

    Add: Searching activity

--- clockify/lib/features/activity/business/usecases/get_activity_by_description.dart
+++ clockify/lib/features/activity/business/usecases/get_activity_by_description.dart
@@ -1,12 +1,14 @@
+import 'package:clockify/core/errors/failure.dart';
 import 'package:clockify/features/activity/business/entities/activity_entity.dart';
 import 'package:clockify/features/activity/business/repositories/activity_repository.dart';
+import 'package:dartz/dartz.dart';
 
 class GetActivityByDescription {
   final ActivityRepository repository;
 
   GetActivityByDescription(this.repository);
 
-  Future<List<ActivityEntity>> call(String description) async {
+  Future<Either<Failure, List<ActivityEntity>>> call(String description) async {
     return repository.getActivityByDescription(description);
   }
 }
