8147265182657ce2d21c190697054ca2eb945ada
itzKv <kenkenku6@gmail.com>
2025-03-25 10:07:51 +0700

    Modify: endpoint baseURL

--- clockify/lib/core/network/api_endpoints.dart
+++ clockify/lib/core/network/api_endpoints.dart
@@ -1,5 +1,5 @@
 class ApiEndpoints {
-  static const String baseUrl = "https://cb52-2001-448a-90e0-210c-ed99-1d77-3f1f-343.ngrok-free.app/api/v1";
+  static const String baseUrl = "https://clocklify-api.onrender.com//api/v1";
   static const String login = "$baseUrl/user/login";
   static const String register = "$baseUrl/user/register";
   static const String verifyEmail = "$baseUrl/user/verifyemail";
