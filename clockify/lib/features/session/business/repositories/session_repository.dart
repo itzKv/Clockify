83effc0773ad1ee75a8e34129efdf4e99c20b4f5
itzKv <kenkenku6@gmail.com>
2025-03-27 10:07:35 +0700

    Modify: Move session to /core

--- clockify/lib/features/session/business/repositories/session_repository.dart
+++
@@ -1,8 +0,0 @@
-import 'package:clockify/features/session/data/models/session_model.dart';
-
-abstract class SessionRepository {
-  Future<void> saveSession(SessionModel session);
-  Future<SessionModel?> getSession();
-  Future<void> clearSession();
-  // Future<Either<Failure, UserParams>> authenticateUser(String email, {String? password, String? confirmPassword});
-}
