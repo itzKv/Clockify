e86ad733bc989d39868b391c3e02d9c74697b184
itzKv <kenkenku6@gmail.com>
2025-03-27 10:08:23 +0700

    Modify: Move Directory

---
+++ clockify/lib/core/services/location_service.dart
@@ -0,0 +1,34 @@
+import 'package:geolocator/geolocator.dart';
+
+class LocationService {
+  Future<Position> getCurrentPosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Check if location services are enabled
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error("Location services are disabled.");
+    }
+
+    // Check location permission
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error("Location permissions are denied.");
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error("Location permission is permanently denied.");
+    }
+
+    // Get the location
+    return await Geolocator.getCurrentPosition(
+      locationSettings: LocationSettings(
+        accuracy: LocationAccuracy.high
+      )
+    );
+  }
+}
